2ce3f5b860e263d48f06d57d561c58af74a677e5
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 14:22:20 +0900

    Feat: Create, Read 추가

--- src/main/java/com/board/board/Controller/PostController.java
+++ src/main/java/com/board/board/Controller/PostController.java
@@ -1,7 +1,81 @@
 package com.board.board.Controller;
 
+import com.board.board.Dto.RequestDto;
+import com.board.board.Dto.ResponseDto;
+import com.board.board.Entity.Post;
+import com.board.board.Dto.RequestDto;
+import com.board.board.Dto.ResponseDto;
+import com.board.board.Entity.Post;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class PostController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PostController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping("/memos")
+    public ResponseDto createMemo(@RequestBody RequestDto requestDto) {
+        // RequestDto -> Entity
+        Post post = new Post(requestDto);
+
+        // DB 저장
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
+
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update( con -> {
+                    PreparedStatement preparedStatement = con.prepareStatement(sql,
+                            Statement.RETURN_GENERATED_KEYS);
+
+                    preparedStatement.setString(1, post.getTitle());
+                    preparedStatement.setString(2, post.getName());
+                    preparedStatement.setString(3, post.getPW());
+                    preparedStatement.setString(4, post.getContents());
+                    return preparedStatement;
+                },
+                keyHolder);
+
+        // DB Insert 후 받아온 기본키 확인
+        Long id = keyHolder.getKey().longValue();
+        post.setId(id);
+
+        // Entity -> ResponseDto
+        ResponseDto memoResponseDto = new ResponseDto(post);
+
+        return memoResponseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<ResponseDto> getPosts() {
+        // DB 조회
+        String sql = "SELECT * FROM memo";
+
+        return jdbcTemplate.query(sql, new RowMapper<ResponseDto>() {
+            @Override
+            public ResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
+                Long id = rs.getLong("id");
+                String title = rs.getString("title");
+                String name = rs.getString("name");
+                String PW = rs.getString("PW");
+                String contents = rs.getString("contents");
+                return new ResponseDto(id, title, name, PW , contents);
+            }
+        });
+    }
 
 }
