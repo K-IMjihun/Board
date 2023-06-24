75e1f9a1b2cbebca5441512279b3d81244b9e30f
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 14:22:51 +0900

    Feat: RequestDto 추가

--- src/main/java/com/board/board/Dto/RequestDto.java
+++ src/main/java/com/board/board/Dto/RequestDto.java
@@ -1,4 +1,14 @@
 package com.board.board.Dto;
+import com.board.board.Entity.Post;
+import lombok.Getter;
 
+@Getter
 public class RequestDto {
+    private Long id;
+    private String title;
+    private String name;
+    private String PW;
+    private String contents;
+
+
 }
