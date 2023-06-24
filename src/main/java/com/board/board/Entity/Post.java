758d2ac61225a3fb3579bae7664c38a2d6adef9e
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 14:21:48 +0900

    Feat: Entity 생성자 추가

--- src/main/java/com/board/board/Entity/Post.java
+++ src/main/java/com/board/board/Entity/Post.java
@@ -1,6 +1,7 @@
 package com.board.board.Entity;
 
 
+import com.board.board.Dto.RequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +10,18 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class Post {
+    private Long id;
     private String title;
     private String name;
     private String PW;
     private String contents;
 
+    public Post(RequestDto requestDto) {
+        this.id = requestDto.getId();
+        this.title = requestDto.getTitle();
+        this.name = requestDto.getName();
+        this.PW = requestDto.getPW();
+        this.contents = requestDto.getContents();
+
+    }
 }
