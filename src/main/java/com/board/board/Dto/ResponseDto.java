156605275713424b4917fa4b4966e58e036fe230
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 19:34:34 +0900

    Refactor: 필요없는 생성자 삭제 및 어노테이션 변경

--- src/main/java/com/board/board/Dto/ResponseDto.java
+++ src/main/java/com/board/board/Dto/ResponseDto.java
@@ -1,29 +1,22 @@
 package com.board.board.Dto;
 
 import com.board.board.Entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
+@Data
+@AllArgsConstructor
 public class ResponseDto {
     private Long id;
     private String title;
     private String name;
-    private String PW;
     private String contents;
-
-    public ResponseDto(Long id, String title, String name, String PW, String contents) {
-        this.id = id;
-        this.title = title;
-        this.name = name;
-        this.PW = PW;
-        this.contents = contents;
-    }
 
     public ResponseDto(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.name = post.getName();
-        this.PW = post.getPW();
         this.contents = post.getContents();
     }
 }
