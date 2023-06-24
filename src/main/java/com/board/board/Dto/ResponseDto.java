4986de26f1846d78cba771629c71228731d7e2cf
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 14:23:12 +0900

    Feat: ResponseDto 추가

--- src/main/java/com/board/board/Dto/ResponseDto.java
+++ src/main/java/com/board/board/Dto/ResponseDto.java
@@ -1,4 +1,31 @@
 package com.board.board.Dto;
 
+import com.board.board.Entity.Post;
+import lombok.Getter;
+
+@Getter
 public class ResponseDto {
+    private Long id;
+    private String title;
+    private String name;
+    private String PW;
+    private String contents;
+
+    public ResponseDto(Long id, String title, String name, String PW, String contents) {
+        this.id = id;
+        this.title = title;
+        this.name = name;
+        this.PW = PW;
+        this.contents = contents;
+    }
+
+    public ResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.name = post.getName();
+        this.PW = post.getPW();
+        this.contents = post.getContents();
+    }
 }
+
+
