3adfba99b883262acca6eae9d936f046f1843eb7
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-24 19:33:48 +0900

    Refactor: 어노테이션 변경 및 PW -> password 변경

--- src/main/java/com/board/board/Dto/RequestDto.java
+++ src/main/java/com/board/board/Dto/RequestDto.java
@@ -1,13 +1,13 @@
 package com.board.board.Dto;
 import com.board.board.Entity.Post;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
+@Data
 public class RequestDto {
-    private Long id;
     private String title;
     private String name;
-    private String PW;
+    private String password;
     private String contents;
 
 
