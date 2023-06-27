082d39a1723b4760bd460febc2ac6a77d7d5a8a0
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-27 13:58:21 +0900

    Refactor: import 정리

--- src/main/java/com/board/board/Dto/PostRequestDto.java
+++ src/main/java/com/board/board/Dto/PostRequestDto.java
@@ -1,14 +1,10 @@
 package com.board.board.Dto;
-import com.board.board.Entity.Post;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
-public class RequestDto {
+public class PostRequestDto {
     private String title;
     private String name;
     private String password;
     private String contents;
-
-
 }
