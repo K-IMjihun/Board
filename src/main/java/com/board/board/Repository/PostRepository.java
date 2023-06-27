ff1634ef30ee950d0c5e5ee0ee7a08587bb20f09
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-27 13:57:32 +0900

    Feat: 시간순 역정렬로 반환하는 List 구현

---
+++ src/main/java/com/board/board/Repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.board.board.Repository;
+
+import com.board.board.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByModifiedAtDesc();
+
+}
