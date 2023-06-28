ba36717b4c328b92fd9ec19fc1bc1d6db8d136ac
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-28 13:59:45 +0900

    Refactor: username이 primaryKey값을 가지게 수정

--- src/main/java/com/board/board/Entity/User.java
+++ src/main/java/com/board/board/Entity/User.java
@@ -14,10 +14,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
     @Pattern(regexp = "[a-z0-9]+", message = "소문자 a~z와 숫자 0~9만 사용할 수 있습니다.")
     @Size(min = 4, max = 10, message = "길이는 4글자 이상, 10글자 이하여야 합니다.")
     private String username;
