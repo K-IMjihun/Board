481f79f567a07e841e3fedde71df26f2f579ac7c
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-28 13:51:32 +0900

    Refactor: 클래스명 변경(PasswordRequestDto > DeleteRequestDto)

--- src/main/java/com/board/board/Dto/PasswordRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.board.board.Dto;
-
-import lombok.Data;
-
-@Data
-public class PasswordRequestDto {
-        private String password;
-}
---
+++ src/main/java/com/board/board/Dto/PostDto/DeleteRequestDto.java
@@ -0,0 +1,8 @@
+package com.board.board.Dto.PostDto;
+
+import lombok.Data;
+
+@Data
+public class DeleteRequestDto {
+        private String password;
+}
