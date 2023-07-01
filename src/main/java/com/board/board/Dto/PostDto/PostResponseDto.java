dc504d7f16a51a4bd568fad55484c501554f348d
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-01 15:38:43 +0900

    Refactor: this.username 제거

--- src/main/java/com/board/board/Dto/PostDto/PostResponseDto.java
+++ src/main/java/com/board/board/Dto/PostDto/PostResponseDto.java
@@ -19,7 +19,6 @@ public class PostResponseDto {
     public PostResponseDto(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
-        this.username = post.getUsername();
         this.contents = post.getContents();
         this.createdAt = post.getCreatedAt();
         this.modifiedAt = post.getModifiedAt();
