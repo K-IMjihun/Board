337e49a87be7201724e6a6423c30b6365b8eeea5
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-04 10:37:01 +0900

    Feat: 게시글 조회 시 권한을 요구하지 않도록 수정

--- src/main/java/com/board/board/Controller/PostController.java
+++ src/main/java/com/board/board/Controller/PostController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/post")
 public class PostController {
-
     private final PostService postService;
 
     public PostController(PostService postService){
@@ -21,34 +19,32 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping
+    @PostMapping("/api/post")
     public PostResponseDto createBoard(ServletRequest request, @RequestBody PostRequestDto postRequestDto) {
         // RequestDto -> Entity
         return postService.createPost(request, postRequestDto);
     }
 
-
-
     //- 제목, 작성자명, 작성 내용, 작성 날짜를 조회하기
     //- 작성 날짜 기준 내림차순으로 정렬하기
-    @GetMapping
+    @GetMapping("/apicm/post")
     public List<PostResponseDto> getPosts() {
         // DB 조회
        return postService.getPosts();
     }
-    @GetMapping("/{id}")
+    @GetMapping("/apicm/post/{id}")
     public PostResponseDto getPost(@PathVariable Long id){
         return postService.getPost(id);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/api/post/{id}")
     public PostResponseDto updatePost(ServletRequest request, @PathVariable(value = "id") Long id, @RequestBody PostRequestDto postRequestDto) {
         // 해당 메모가 DB에 존재하는지 확인
 
         return postService.putPost(request, id, postRequestDto);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/post/{id}")
     public DeleteResponseDto deleteMemo(ServletRequest request, @PathVariable Long id, @RequestBody DeleteRequestDto deleteRequestDto) {
         return postService.deletePost(request, id, deleteRequestDto);
     }
