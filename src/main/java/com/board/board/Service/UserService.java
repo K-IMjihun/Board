966fd160807937a9d7b3c21392ebf1d24b52d075
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-05 15:14:55 +0900

    Feat: ROLE 제거 및 Login 기능 구현

--- src/main/java/com/board/board/Service/UserService.java
+++ src/main/java/com/board/board/Service/UserService.java
@@ -1,14 +1,20 @@
 package com.board.board.Service;
 
+import com.board.board.Dto.ResultResponseDto;
+import com.board.board.Dto.UserDto.LoginRequestDto;
 import com.board.board.Dto.UserDto.SignupRequestDto;
 import com.board.board.Entity.User;
-import com.board.board.Entity.UserRoleEnum;
+import com.board.board.Jwt.JwtUtil;
 import com.board.board.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 @Service
@@ -19,10 +25,11 @@ public class UserService {
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
     private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Transactional
-    public void signup(SignupRequestDto requestDto) {
+    public ResultResponseDto signup(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
         String password = passwordEncoder.encode(requestDto.getPassword());
 
@@ -31,14 +38,33 @@ public class UserService {
         if (checkUsername.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
         }
-
-        // 사용자 ROLE 확인
-        // 사용자 확인을 위해 우선 일반 사용자 권한으로 코드 실행
-        UserRoleEnum role = UserRoleEnum.USER;
-
-        // 일반 사용자 등록
-        User user = new User(username, password, role);
+        // 사용자 등록
+        User user = new User(username, password);
         log.info("user: {}", user);
         userRepository.save(user);
+        ResultResponseDto resultResponseDto = new ResultResponseDto("회원가입 성공", 200L);
+
+        return resultResponseDto;
+    }
+
+    public ResponseEntity<ResultResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+
+        // 사용자 확인
+        User user =  userRepository.findByUsername(username).orElseThrow(() ->
+                new IllegalArgumentException("등록되지 않은 회원입니다."));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("비밀번호가 맞지 않습니다.");
+        }
+
+        String token = jwtUtil.createToken(user.getUsername());
+
+        HttpHeaders headers = new HttpHeaders();
+        // 헤더 생성
+        headers.add(JwtUtil.AUTHORIZATION_HEADER, token);
+
+        return new ResponseEntity<>(new ResultResponseDto("로그인 성공!", 200L), headers, HttpStatus.OK);
     }
 }
