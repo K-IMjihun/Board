3dc97f92a9c6b61e0ac21bc361a0dea47b18a351
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-05 15:11:56 +0900

    Comment: 주석 변경

--- src/main/java/com/board/board/Entity/Timestamped.java
+++ src/main/java/com/board/board/Entity/Timestamped.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public abstract class Timestamped {
 
     @CreatedDate
-    @Column(updatable = false)  // 업데이트를 안함
+    @Column(updatable = false)  // 등록시간
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
