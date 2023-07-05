5a518da9df50c542b0c83a9aca2db59851c2aa64
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-05 15:13:26 +0900

    Feat: 요구에 맞게 return값 변경

--- src/main/java/com/board/board/Controller/UserController/UserController.java
+++ src/main/java/com/board/board/Controller/UserController/UserController.java
@@ -1,21 +1,20 @@
 package com.board.board.Controller.UserController;
 
+import com.board.board.Dto.ResultResponseDto;
+import com.board.board.Dto.UserDto.LoginRequestDto;
 import com.board.board.Dto.UserDto.SignupRequestDto;
 import com.board.board.Service.UserService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserController {
 
@@ -36,12 +35,12 @@ public class UserController {
     }
 
     @PostMapping("/user/login")
-    public String login() {
-        return "index";
+    public ResponseEntity<ResultResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return userService.login(loginRequestDto);
     }
 
     @PostMapping("/user/signup")
-    public String signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    public ResultResponseDto signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
 
         log.info(requestDto.getUsername() + " , " + requestDto.getPassword());
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
@@ -49,12 +48,8 @@ public class UserController {
             for (FieldError fieldError : bindingResult.getFieldErrors()) {
                 log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
             }
-
-            return "redirect:/api/user/signup";
+            throw new RuntimeException("데이터 유효성 검사 오류");
         }
-
-        userService.signup(requestDto);
-
-        return "redirect:/api/user/login-page";
+        return userService.signup(requestDto);
     }
 }
