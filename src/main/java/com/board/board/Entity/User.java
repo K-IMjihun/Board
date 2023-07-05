0d3c8bf06a6360e2cfdeb856e0fb92b9726d3df4
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-05 15:12:28 +0900

    Feat: Id값을 username에서 Long id로 변경

--- src/main/java/com/board/board/Entity/User.java
+++ src/main/java/com/board/board/Entity/User.java
@@ -1,6 +1,7 @@
 package com.board.board.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,18 +14,17 @@ import lombok.ToString;
 @ToString
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 
-    //사용자의 권한
-    @Column(nullable = false)
-    @Enumerated(value = EnumType.STRING)
-    private UserRoleEnum role;
 
 
-    public User(String username, String password, UserRoleEnum role) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.role = role;
     }
 }
